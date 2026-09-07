b7fcc81e91032c570236f099a19669c4f1f1b2e6
Shireen Kachroo <kachrooshireen3@gmail.com>
2026-09-07 15:39:35 +0530

    feat: created GET endpoint for returning all students in the dummy DB

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 # python -m uvicorn main:app --reload
 
+students = {101: "Shireen", 102: "Anshika", 103: "Dhruvi", 104: "Khushi"}
 app = FastAPI()
 @app.get("/")
 def print_conn():
     return {"message": "API chal gayi balle balle"}
+
+@app.get("/students")
+def get_students():
+    return {"students": students}
 
