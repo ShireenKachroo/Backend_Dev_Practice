b1376326587db8a152ccf9e3a518c16b1401785b
Shireen Kachroo <kachrooshireen3@gmail.com>
2026-09-22 12:39:55 +0530

    add: user schmea

--- schemas.py
+++ schemas.py
@@ -32,3 +32,16 @@ class DepartmentResponse(BaseModel):
 
     class Config:
         from_attributes = True
+
+class UserCreate(BaseModel):
+    username: str
+    password: str
+
+
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    role: str
+
+    class Config:
+        from_attributes = True
