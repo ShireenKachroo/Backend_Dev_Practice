cabda24ac02c00cea2748e797634d75545f15a4b
Shireen Kachroo <kachrooshireen3@gmail.com>
2026-09-22 11:20:19 +0530

    add placeholder for database_url

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 
-DATABASE_URL = "postgresql://postgres:1234@localhost:5432/student_management"
+DATABASE_URL = "postgresql://postgres:YOUR_PSWRD@localhost:5432/student_management"
 
 engine = create_engine(DATABASE_URL)
 
