7f1e2b38e80a4d50890195214495a73ba3a8f277
Shireen Kachroo <kachrooshireen3@gmail.com>
2026-09-23 15:33:11 +0530

    add: hashing + JWT

--- auth.py
+++ auth.py
@@ -1,13 +1,43 @@
 import bcrypt
 
+from datetime import datetime, timedelta, timezone
+from jose import jwt
+
+
+SECRET_KEY = "change-this-later"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
 
 def hash_password(password: str):
     password_bytes = password.encode("utf-8")
-    return bcrypt.hashpw(password_bytes, bcrypt.gensalt()).decode("utf-8")
+    return bcrypt.hashpw(
+        password_bytes,
+        bcrypt.gensalt()
+    ).decode("utf-8")
 
 
 def verify_password(password: str, password_hash: str):
     password_bytes = password.encode("utf-8")
     hash_bytes = password_hash.encode("utf-8")
 
-    return bcrypt.checkpw(password_bytes, hash_bytes)
+    return bcrypt.checkpw(
+        password_bytes,
+        hash_bytes
+    )
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.now(timezone.utc) + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+
+    to_encode.update({"exp": expire})
+
+    return jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
